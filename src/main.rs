ffb569136a3af06baa680f2a6cbac6699955968e
Trenton <starkbewegung@hotmail.com>
2025-05-15 14:35:54 -0700

    started implementing the real REST api I'd like to have on the site

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,33 @@ use std::{
 };
 use tokio::sync::RwLock;
 
+struct User {
+    id: i32,
+    username: String,
+}
+
+impl User {
+    fn new(id: i32, username: String) -> Self {
+        User { id, username }
+    }
+    
+    //during implementation, may have to change the user_list to mutable 
+    fn format(&self, to_add: &User, list: &mut Vec<String>) {
+        list.push(format!("   - ID: {} | Username: {}", to_add.id, to_add.username))
+    }
+}
+
 struct AppState {
-    foo_response: RwLock<String>
+    user_list: RwLock<Vec<User>>
 }
 
 #[tokio::main]
 async fn main() {
-    let shared_state = Arc::new(AppState {foo_response: RwLock::new(String::from("Foo"))});
+    let shared_state = Arc::new(AppState { user_list: RwLock::new(Vec::with_capacity(10))});
     let app = Router::new()
         .route("/", get(root))
-        .route("/foo", get(get_foo))
-        .route("/change_foo/{new_foo}", post(post_foo))
-        .route("/foo/bar", get(foo_bar_stranger))
-        .route("/foo/bar/{user_name}", get(foo_bar))
+        .route("/users", get(users))
+        // .route("/users/add", post())
         .fallback(unknown_path)
         .with_state(shared_state);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
@@ -28,25 +42,28 @@ async fn root(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Html<String> {
     format!("<h1>Hello, World!</h1><h2> You are connecting from: {}</h2>", addr).into()
 }
 
-async fn get_foo(State(state): State<Arc<AppState>>) -> Html<String> {
-    let foo_status = state.foo_response.read().await;
-    format!("<h1>Can I get a foo? {}</h1>", *foo_status).into()
-}
+// async fn get_foo(State(state): State<Arc<AppState>>) -> Html<String> {
+//     let foo_status = state.foo_response.read().await;
+//     format!("<h1>Do you know what a foo is? {}</h1>", *foo_status).into()
+// }
+// 
+// async fn post_foo(State(state): State<Arc<AppState>>, Path(new_foo): Path<String>) -> Html<String>{
+//     let mut foo_status = state.foo_response.write().await;
+//     *foo_status = new_foo.clone();
+//     format!("<h1>New foo is {}!</h1>",  *foo_status).into()
+// }
+// 
+// async fn foo_bar_stranger() -> Html<String> {
+//     Html(String::from("Hello, stranger!"))
+// }
+// 
+// async fn foo_bar(path : Option<Path<String>>) -> impl IntoResponse {
+//     let user_name= path.unwrap();
+//     format!("Hello, {}!", user_name.0)
+// }
 
-async fn post_foo(State(state): State<Arc<AppState>>, Path(new_foo): Path<String>) -> Html<String>{
-    let mut foo_status = state.foo_response.write().await;
-    println!("Old foo: {}", *foo_status);
-    *foo_status = new_foo.clone();
-    format!("<h1>New foo is {}!</h1>",  *foo_status).into()
-}
-
-async fn foo_bar_stranger() -> Html<String> {
-    Html(String::from("Hello, stranger!"))
-}
-
-async fn foo_bar(path : Option<Path<String>>) -> impl IntoResponse {
-    let user_name= path.unwrap();
-    format!("Hello, {}!", user_name.0)
+async fn users(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    
 }
 
 async fn unknown_path() -> Redirect {
