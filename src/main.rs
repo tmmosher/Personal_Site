b307c2f0e9e29ac8dde61a93279b4ae983f72e39
Trenton <starkbewegung@hotmail.com>
2025-07-17 15:29:46 -0700

    Completed database functionality addition. Pagination is next goal.

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,8 @@ async fn main() {
     axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.expect("Serving failed");
 }
 
-// as this is a startup function, this uses unsafe functions like expect().
+/// Creates or connects to database needed for internal application state.
+// as this is a function run at startup, this uses unsafe functions like expect() and can fail.
 async fn bootstrap() -> Arc<AppState> {
     let database = match dotenvy::dotenv() {
         Ok(_buf) => {
@@ -103,12 +104,14 @@ async fn bootstrap() -> Arc<AppState> {
         .execute(query).await.expect("Failed to create user and post table in 'bootstrap()'");
     Arc::new(AppState { read_pool: read_conn, write_pool: write_conn, per_page: 32 })
 }
+
+/// Home page
 async fn root(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Response {
     let mut context = tera::Context::new();
     context.insert("adr", &addr.to_string());
     let page = TEMPLATES.render("index.html", &context);
     match page {
-        // return a tuple parsable to an axum::response
+        // return a tuple parsable to an axum::Response
         Ok(page) => {
             (
                 StatusCode::OK,
@@ -126,10 +129,17 @@ async fn root(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Response {
     }
 }
 
-async fn users(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+async fn users(State(state): State<Arc<AppState>>) -> Response {
     let mut context = tera::Context::new();
-    //TODO Implement DB reading for users
-    // users = //here!;
+    if let Ok(users) = get_users_by_pagination(state).await {
+        context.insert("users", &users);
+    } else {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            [("Content-Type", "text/html")],
+            Body::from("<h1>Internal server error: Cannot display users.<h1>")
+        ).into_response()
+    }
     //TODO pagination
     context.insert("page_no", &1);
     let page = TEMPLATES.render("users.html", &context);
@@ -140,22 +150,20 @@ async fn users(State(state): State<Arc<AppState>>) -> impl IntoResponse {
                 StatusCode::OK,
                 [("Content-Type", "text/html")],
                 Body::from(page)
-            )
+            ).into_response()
         }
         Err(_e) => {
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 [("Content-Type", "text/html")],
                 Body::from("<h1>Internal server error: Cannot display users.<h1>")
-            )
+            ).into_response()
         }
     }
 }
 
-/*
-    API endpoint to return users as a JSON list.
- */
-async fn get_users(state: State<Arc<AppState>>) -> impl IntoResponse {
+///    API endpoint to return users as a JSON list.
+async fn get_users(State(state): State<Arc<AppState>>) -> Response {
     let body = match get_users_by_pagination(state).await {
         Ok(t) => to_value(t),
         Err(_e) => to_value("")
@@ -166,34 +174,37 @@ async fn get_users(state: State<Arc<AppState>>) -> impl IntoResponse {
                 StatusCode::OK,
                 [("Content-Type", "application/json")],
                 Body::from(body.to_string())
-            )
+            ).into_response()
         }
         Err(_) => {
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 [("Content-Type", "text/plain")],
                 Body::from("Internal server error")
-            )
+            ).into_response()
         }
     }
 }
 
 //TODO implement 'pagination' part of 'get_users_by_pagination'
-async fn get_users_by_pagination (state: State<Arc<AppState>>) -> Result<Vec<User>, sqlx::error::Error> {
-    sqlx::query_as("SELECT * FROM users ORDER BY last_online ASC LIMIT $1")
+async fn get_users_by_pagination (state: Arc<AppState>) -> Result<Vec<User>, sqlx::error::Error> {
+    sqlx::query_as("SELECT * FROM user_table ORDER BY username ASC LIMIT $1")
         .bind(state.per_page)
         .fetch_all(&state.read_pool)
         .await
 }
 
+/// Handles detailed account creation and database access. Returns either a response ready to be sent
+/// or an error to fn 'post_user'.
 async fn post_user_body(state: State<Arc<AppState>>, result: Result<User, (StatusCode, String)>)
     -> Result<impl IntoResponse, anyhow::Error> {
-    //unwraps the user
+    //HeaderMap is more readable here, as responses get ugly when using a list of tuples
+    // for multiple headers.
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_str("text/plain")?);
     let new_user = match result {
         Ok(user) => user,
-        // Despite being an Err case, this is a valid response to return to the user.
+        //Despite being an Err case, this is a valid response to bubble up to fn post_user
         Err((code, reason)) => {
             return Ok((
                     code,
@@ -214,9 +225,9 @@ async fn post_user_body(state: State<Arc<AppState>>, result: Result<User, (Statu
             match anyhow.to_string().as_str() {
                 "No users found with this username." => {
                     insert_user(&new_user, &state).await?;
-                    //TODO change from localhost:3000 when moving away from local testing
-                    //headers.insert("Location", HeaderValue::from_str(format!("https://tmmosher.com/user/{}", new_user.username).as_str())?);
-                    headers.insert("Location", HeaderValue::from_str(format!("https://localhost:3000/user/{}", new_user.username).as_str())?);
+                    //change between localhost:3000 and production domain for local testing and vice versa.
+                    headers.insert("Location", HeaderValue::from_str(format!("https://tmmosher.com/user/{}", new_user.username).as_str())?);
+                    //headers.insert("Location", HeaderValue::from_str(format!("https://localhost:3000/user/{}", new_user.username).as_str())?);
                     Ok((
                         StatusCode::CREATED,
                         headers,
@@ -231,13 +242,15 @@ async fn post_user_body(state: State<Arc<AppState>>, result: Result<User, (Statu
     }
 }
 
+/// POST request handler for account creation.
 async fn post_user(state: State<Arc<AppState>>, result: Result<Json<Value>, JsonRejection>)
     -> Response {
+    // unwraps user information from the POST body
     let user_status = match result {
         Ok(Json(value)) => match value.get("username") {
             // if the extractor passes and a username field exists, evaluates to a new user.
             // do note, dear reader, that this doesn't do any pattern checking for a username.
-            // I should probably add size limits later, but for now this will suffice.
+            // I should definitely add size mins/maxes later, but for now this will suffice.
             // For obvious security reasons only users (role lvl 2) can be created via the API.
             Some(name) => Ok(User::new(name.to_string(), 2)),
             // JSON format is valid but doesn't contain the required field 'username'
@@ -251,12 +264,12 @@ async fn post_user(state: State<Arc<AppState>>, result: Result<Json<Value>, Json
         }
     };
     match post_user_body(state, user_status).await {
-        // matches all defined behavior, including 'bad' responses (BAD_REQUEST, bytes rejection,
+        // matches all expected behavior including good and bad responses (bad request, bytes rejection,
         // improper JSON, etc.)
         Ok(v) => {
             v.into_response()
         },
-        // matches all undefined behavior, essentially just server errors.
+        // matches all uncaught behavior. This should not be encountered often (ideally ever).
         Err(_) => {
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
@@ -267,14 +280,13 @@ async fn post_user(state: State<Arc<AppState>>, result: Result<Json<Value>, Json
     }
 }
 
-//TODO refactor to an option rather than a result. Works as-is but code clarity suffers
 async fn select_by_username(username: &str,  state: &State<Arc<AppState>>) -> Result<User, anyhow::Error> {
     let read_conn = &state.read_pool;
-    let p_stmnt = sqlx::query_as("SELECT * FROM users WHERE username = $1 LIMIT 1")
+    let p_stmnt = sqlx::query_as("SELECT * FROM user_table WHERE username = $1 LIMIT 1")
         .bind(username.to_string())
         .fetch_optional(read_conn)
         .await;
-    // manually unwrap p_stmnt for better error handling response
+    // nested unwrap of p_stmnt for better error handling response
     match p_stmnt {
         Ok(v) => {
             match v {
@@ -288,9 +300,13 @@ async fn select_by_username(username: &str,  state: &State<Arc<AppState>>) -> Re
     }
 }
 
+/// Inserts a user from memory into persistent storage.
+/* Result is used rather than Option for unpacking with the '?' 
+    operator to help code readability during the query. Furthermore, Axum **really** doesn't like 
+    returning Options, but it seems to be happy with Results.*/
 async fn insert_user(user: &User, state: &State<Arc<AppState>>) -> Result<bool, anyhow::Error> {
     let write_conn = &state.write_pool;
-    let insert_statement = sqlx::query("INSERT INTO users (username, last_online, created, role)
+    let insert_statement = sqlx::query("INSERT INTO user_table (username, last_online, created, role)
         VALUES ($1, $2, $3, $4)")
         .bind(user.username.clone())
         .bind(user.last_online.to_string())
